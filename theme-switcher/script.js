6dcf61f98f8b35036333f6d07e34a99d78668665
LEvans1s <LEvans1s@capetigers.com>
2023-02-15 10:41:13 -0600

    your mom

---
+++ theme-switcher/script.js
@@ -0,0 +1,18 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const burgerButton = document.getElementById('burger');
+const body = document.body;
+
+darkButton.onClick = () => {
+
+};
+
+
+lightButton.onClick = () => {
+
+};
+
+
+burgerButton.onClick = () => {
+
+};
