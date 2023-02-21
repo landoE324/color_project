88f017fcc74fe9334fd7bdf805e583342cb9e4e8
LEvans1s <LEvans1s@capetigers.com>
2023-02-21 10:28:18 -0600

    fix javascript links

--- theme-switcher/script.js
+++ theme-switcher/script.js
@@ -1,18 +1,24 @@
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
-const burgerButton = document.getElementById('burger');
+const burgerButton = document.getElementById('sandwich');
 const body = document.body;
 
-darkButton.onClick = () => {
-
+darkButton.onclick = () => {
+body.classList.remove('light');
+body.classList.remove('sandwich');
+body.classList.add('dark');
 };
 
 
-lightButton.onClick = () => {
-
+lightButton.onclick = () => {
+    body.classList.remove('dark');
+    body.classList.remove('sandwich');
+    body.classList.add('light');
 };
 
 
-burgerButton.onClick = () => {
-
-};
+burgerButton.onclick = () => {
+    body.classList.remove('light');
+    body.classList.remove('dark');
+    body.classList.add('sandwich');
+}
